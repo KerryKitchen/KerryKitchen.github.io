5080efd8672a9403347cdf291938435716927040
Kerry <kerry.kitchen@hotmail.com>
2022-11-29 12:25:08 +1000

    Added clock code
    Removed some default files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="clock">
+      <div id="t">t</div>
+      <div id="i-o">
+        <div id="hand-0">i</div>
+        <div id="hand-1">o</div>
+        <div id="hand-2">i</div>
+        <div id="hand-3">o</div>
+        <div id="hand-4">i</div>
+        <div id="hand-5">o</div>
+        <div id="hand-6">i</div>
+        <div id="hand-7">o</div>
+        <div id="hand-8">i</div>
+        <div id="hand-9">o</div>
+        <div id="hand-10">i</div>
+        <div id="hand-11">o</div>
+      </div>
+      <div id="ck">
+        <div>c</div>
+        <div>k</div>
+      </div>
     </div>
   );
 }
